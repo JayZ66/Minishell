ac1dd560d72f7c17237ba945abceb8ed69e8daad
JayZ66 <jezaguerin.pro@gmail.com>
2024-03-22 19:37:50 +0100

    Starting to read input and split it for nodes

--- extract_cmd.c
+++ extract_cmd.c
@@ -18,4 +18,42 @@ Tout ce qui est entre on met dans le noeud (entiereté de la cmd)
 On split ensuite
 */
 
+t_token	*extract_cmd(t_token **token, char *cmd_line)
+{
+	size_t	i;
+	size_t	start;
+	char	*cmd;
+	t_token	*new;
 
+	i = 0;
+	new = NULL;
+	while (cmd_line[i] && cmd_line[i] != '\n')
+	{
+		while (cmd_line[i] && cmd_line[i] == 32)
+			i++;
+		start = i;
+		while ((cmd_line[i] && cmd_line[i] != '|'))
+			i++;
+		if (i > start)
+		{
+			cmd = ft_strndup(cmd_line + start, i - start);
+			printf("Taille envoyée : %ld\n", i - start);
+			printf("cmd après strndup : %s\n", cmd);
+			if (!cmd)
+				return(perror("Can't get cmd\n"), NULL);
+			new = init_node(cmd);
+			if (!new)
+				return(perror("Can't init my node\n"), free(cmd), NULL);
+			add_back(token, new);
+			printf("Contenu de mon noeud : %s\n", new->content);
+			free(cmd);
+		}
+		i++;
+	}
+	return (*token);
+}
+
+
+// Je crée une str, avec strdup, où je récupère jusqu'au pipe.
+// Une fois que j'ai récupéré mon bout de str. je l'envoie pour créer un new node.
+// Ensuite je l'envoie dans add_back pour l'ajouter au bout de ma liste chaînée.
--- main.c
+++ main.c
@@ -37,45 +37,6 @@ TOKENISATION :
 2. Historique, signal, buildins, etc.
 */
 
-t_token	*extract_cmd(t_token **token, char *cmd_line)
-{
-	size_t	i;
-	size_t	start;
-	char	*cmd;
-	t_token	*new;
-
-	i = 0;
-	new = NULL;
-	while (cmd_line[i] && cmd_line[i] != '\n')
-	{
-		while (cmd_line[i] && cmd_line[i] == 32)
-			i++;
-		start = i;
-		while ((cmd_line[i] && cmd_line[i] != '|'))
-			i++;
-		if (i > start)
-		{
-			cmd = ft_strndup(cmd_line + start, i - start);
-			printf("Taille envoyée : %ld\n", i - start);
-			printf("cmd après strndup : %s\n", cmd);
-			if (!cmd)
-				return(perror("Can't get cmd\n"), NULL);
-			new = init_node(cmd);
-			if (!new)
-				return(perror("Can't init my node\n"), free(cmd), NULL);
-			add_back(token, new);
-			printf("Contenu de mon noeud : %s\n", new->content);
-			free(cmd);
-		}
-		i++;
-	}
-	return (*token);
-}
-
-
-// Je crée une str, avec strdup, où je récupère jusqu'au pipe.
-// Une fois que j'ai récupéré mon bout de str. je l'envoie pour créer un new node.
-// Ensuite je l'envoie dans add_back pour l'ajouter au bout de ma liste chaînée.
 
 char	*read_input()
 {
