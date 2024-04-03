5be0e7a25eb18ed9fa629d98875e78a319e4e677
Romain Lambert <lambertromain66270@gmail.com>
2024-04-03 16:01:24 +0200

    les espaces c okay

--- minishell.h
+++ minishell.h
@@ -130,6 +130,7 @@ int	tokenize_simple_quote(t_token **token, char *input, int i);
 t_token    *init_node_separator(Token_type type);
 
 t_clean_token	*clean_arg(t_token **token);
+t_clean_token	*clean_space(t_clean_token **clean_token);
 char	*ft_clean_space(char *content);
 
 
--- parsing_cmd.c
+++ parsing_cmd.c
@@ -6,7 +6,7 @@
 /*   By: romlambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 23:02:21 by romlambe          #+#    #+#             */
-/*   Updated: 2024/04/03 15:20:51 by romlambe         ###   ########.fr       */
+/*   Updated: 2024/04/03 15:58:20 by romlambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,5 +121,78 @@ t_clean_token	*clean_arg(t_token **token)
 		free(arg);
 	}
 
-	return clean_token;
+	return (clean_token);
+}
+
+
+// t_clean_token	*clean_space(t_clean_token **clean_token)
+// {
+// 	int	i;
+// 	int	flag;
+// 	char	*temp;
+
+// 	temp = (char *)malloc(ft_strlen((*clean_token)->content + 1 * sizeof(char)));
+// 	if (!temp)
+// 	{
+// 		perror("erreur memoire");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	while(*clean_token)
+// 	{
+// 		i = 0;
+// 		while ((*clean_token)->content[i])
+// 		{
+// 			if ((*clean_token)->content[i] == ' ' || (*clean_token)->content[i] == '\t')
+// 				flag = 1;
+// 			if (!((*clean_token)->content[i] == ' ' || (*clean_token)->content[i] == '\t'))
+// 			{
+// 				if (flag == 1)
+// 					temp[i] = ' ';
+// 				flag = 0;
+// 				temp[i] = (*clean_token)->content[i];
+// 			}
+// 			i++;
+// 		}
+// 		(*clean_token)->content = temp;
+// 		(*clean_token) = (*clean_token)->next;
+// 	}
+// 	return (*clean_token);
+// }
+
+t_clean_token *clean_space(t_clean_token **clean_token)
+{
+    while (*clean_token)
+    {
+        int i = 0;
+        int flag = 0;
+        char *temp = (char *)malloc(strlen((*clean_token)->content) + 1); // Allouer de la mémoire pour temp
+
+        if (temp == NULL) {
+            // Gestion de l'erreur d'allocation mémoire
+            perror("Erreur d'allocation mémoire");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(temp, 0, strlen((*clean_token)->content) + 1); // Initialiser temp à une chaîne vide
+
+        for (size_t j = 0; j < ft_strlen((*clean_token)->content); j++)
+        {
+            if ((*clean_token)->content[j] == ' ' || (*clean_token)->content[j] == '\t')
+                flag = 1;
+            if (!((*clean_token)->content[j] == ' ' || (*clean_token)->content[j] == '\t'))
+            {
+                if (flag == 1)
+                {
+                    temp[i++] = ' ';
+                    flag = 0;
+                }
+                temp[i++] = (*clean_token)->content[j];
+            }
+        }
+        temp[i] = '\0'; // Assurez-vous que la chaîne de caractères est terminée par un caractère nul
+        free((*clean_token)->content); // Libérer la mémoire de l'ancienne chaîne de caractères
+        (*clean_token)->content = temp; // Affecter la nouvelle chaîne de caractères à content
+        *clean_token = (*clean_token)->next;
+    }
+    return *clean_token;
 }
